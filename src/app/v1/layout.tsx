089b98831d0caebda2e55f81b49438580c2788d7
Biswayan Paul <biswayanpaul2015@gmail.com>
2025-07-31 18:19:34 +0530

    seo2

--- src/app/v1/layout.tsx
+++ src/app/v1/layout.tsx
@@ -6,6 +6,11 @@ export default function BlogLayout({
     children: React.ReactNode
 }) {
     return <section>
+        <head>
+            {/* Force index and follow manually */}
+            <meta name="robots" content="index, follow" />
+            <meta name="googlebot" content="index, follow" />
+        </head>
         <Navbar />
         {children}
     </section>
